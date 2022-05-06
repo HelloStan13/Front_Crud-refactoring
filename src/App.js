45e2febe491a61a8e4354084e5658cf85be2754b
Adryan <adryanynfante@gmail.com>
2022-05-06 00:52:57 -0500

    front react
    creacion de conexion con api, inicio de orden de componentes de la app todo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import StoreProvider from './store';
+import FormList from './Components/FormList';
+import ListGroup from './Components/ListGroup';
+
+function App() {
+  return <StoreProvider>
+    <br/>
+    <div className="container border border-dark" style={{padding: '2%'}}> 
+      <FormList />
+      <br/>
+      <ListGroup />
+    </div>
+  </StoreProvider>
+}
+
+export default App;
