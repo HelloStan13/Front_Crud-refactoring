4b35973e0e4d476d35f05e175a2ded08fb6848e3
Adryan <adryanynfante@gmail.com>
2022-05-06 09:54:34 -0500

    validacion de nombre del proyecto o grupo
    validacion de nombre OK

--- src/Components/Form_Group.js
+++ src/Components/Form_Group.js
@@ -11,23 +11,33 @@ export default function FormList() {
     const [state, setState] = useState(item);
   
     const onAdd = async (event) => {
-      event.preventDefault();
-  
+      const nombre = document.getElementById("name");
+      if(!nombre.value)
+    {
+      nombre.focus();
+      return false;
+    }else if(nombre.value){
+
+      
       const request = {
         name: state.name,
         id: null
       };
-
+      
       try {
         const groupList = await api.grouplist.addGroupList(request);
         dispatch({ type: "add-item-group", item: groupList });
         setState({ name: "" });
         form.current.reset();
-
+        
+  
       }catch (error){
         console.log(error)
-      }
+      }    
     }
+
+      return true;
+    }     
   
     const onEdit = async (event) => {
       event.preventDefault();
@@ -42,12 +52,27 @@ export default function FormList() {
             const todo = await api.todo.edit(request);
             dispatch({ type: "update-item", item: todo });
             setState({ name: "" });
+            
             form.current.reset();
+            
         } catch(error) {
             console.log(error);
         }
     }
-    
+
+    function validarForm(){
+      const nombre = document.getElementById("name");
+          if(!nombre.value)
+        {
+          alert("El campo nombre es requerido");
+          nombre.focus();
+          return false;
+        }else if(nombre.value){
+          return true;
+        }
+
+    }
+
 
     return (
         <form className="container">
@@ -60,16 +85,16 @@ export default function FormList() {
                   className="form-control col-10"
                   type="text"
                   name="name"
+                  id='name'
                   required="required" 
                   placeholder="Nombre de proyecto"
-
                   defaultValue={item.name}
                   onChange={(event) => {
                   setState({ ...state, name: event.target.value })
                   }} ></input>
-            {item.id && <button className='btn btn-primary' onClick={onEdit}>Actualizar</button>}
+            {item.id && <button className='btn btn-primary' onClick={() => { onEdit();  }}>Actualizar</button>}
             {!item.id && 
-              <button class="btn btn-success btn-sm col-2" onClick={onAdd}>
+              <button class="btn btn-success btn-sm col-2" onClick={() => { onAdd();  }}>
                 Crear proyecto
               </button>}
           </div>
